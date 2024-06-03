02a62ec94dc1df5afcd0bfcc98104f5fd19132d3
Simon Essig <essigsimon@web.de>
2024-06-03 10:26:10 +0200

    Removed old scripts

--- launcher/launch_gripper.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-CONDA_PREFIX=${CONDA_PREFIX:-"$(dirname $(which conda))/../"} \
-LD_LIBRARY_PATH=${CONDA_PREFIX}/lib:${LD_LIBRARY_PATH} \
-launch_gripper.py \
---config-path $PWD/conf \
---config-name gripper_launch.yaml
